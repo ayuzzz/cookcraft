21086aa0a44fda4fac874472b9ebb2ee92a007e8
Ayush Joshi <joshiayush2426@gmail.com>
2025-07-28 12:12:29 +0530

    Added navbar

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./navbar.module.css";
+
+export const Navbar = () =>
+{
+    const navLinks = [
+        {href: "/", label: "Home"},
+        {href: "/log", label: "Log"},
+        {href: "/dashboard", label: "Dashboard"},
+        {href: "/history", label: "History"},
+        {href: "/ask-ai", label: "Ask AI"},
+    ];
+
+    return (
+        <header>
+            <div className="bg-[var(--primary-color)] container mx-auto flex gap-4 items-center justify-between px-4 py-3 shadow-lg">
+                <Image
+                    src="/favicon.ico"
+                    alt="CookCraft Logo"
+                    width={40}
+                    height={40}
+                />
+                <Link href="/" className="text-xl font-bold">
+                    CookCraft
+                </Link>
+
+                <nav className="flex-grow flex gap-6 justify-end px-4 py-3">
+                    {navLinks.map((link) => 
+                        <Link href={link.href} key={link.label} className="text-[var(--primary-text-color)] hover:text-white text-lg font-semibold">
+                            {link.label}
+                        </Link>
+                    )}
+                </nav>
+            </div>
+        </header>
+    );
+}
