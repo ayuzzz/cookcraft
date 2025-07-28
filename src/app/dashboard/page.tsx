c9723dc3e2663b9578f578c2c429c88d9021d997
Ayush Joshi <joshiayush2426@gmail.com>
2025-07-28 16:51:42 +0530

    dashboard-related changes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import SummaryCards from "@/components/dashboard/SummaryCards";
+
+export default function Dashboard() {
+  return (
+    <main>
+      <section className="container mx-auto px-4 py-8">
+        <SummaryCards />
+      </section>
+
+      <section>
+        <h1>Charts Section</h1>
+      </section>
+
+      <section>
+        Action Section
+      </section>
+    </main>
+  );
+}
